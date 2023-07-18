e077ab46188f9f5d6c9e35e2012741686798bfd5
Lewis Sneddon <lewis@lewiscodes.io>
2023-07-18 20:55:40 +0100

    updated seed data with some more deals

--- init-db.sql
+++ init-db.sql
@@ -1,23 +1,26 @@
-CREATE TABLE positions (
-    ticker VARCHAR(255),
+CREATE TABLE positions
+(
+    ticker    VARCHAR(255),
     direction VARCHAR(255),
-    price FLOAT,
-    quantity FLOAT
+    price     FLOAT,
+    quantity  FLOAT
 );
-insert into positions values (
-    'EDV', 'long', 1892.0, 10.0
-);
+insert into positions
+values ('EDV', 'long', 1892.0, 10.0);
+insert into positions
+values ('SDR', 'long', 440.55, 10.0);
+insert into positions
+values ('ABDN', 'long', 222.5, 10.0);
 
-CREATE TABLE technologies (
-  name    VARCHAR(255),
-  details VARCHAR(255)
+CREATE TABLE technologies
+(
+    name    VARCHAR(255),
+    details VARCHAR(255)
 );
-insert into technologies values (
-  'Go', 'An open source programming language that makes it easy to build simple and efficient software.'
-);
-insert into technologies values (
-  'JavaScript', 'A lightweight, interpreted, or just-in-time compiled programming language with first-class functions.'
-);
-insert into technologies values (
-  'MySQL', 'A powerful, open source object-relational database'
-);
+insert into technologies
+values ('Go', 'An open source programming language that makes it easy to build simple and efficient software.');
+insert into technologies
+values ('JavaScript',
+        'A lightweight, interpreted, or just-in-time compiled programming language with first-class functions.');
+insert into technologies
+values ('MySQL', 'A powerful, open source object-relational database');
