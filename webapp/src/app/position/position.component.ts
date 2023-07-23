5e1f79bf53e01b1bebfd5024314815e2f8e08142
Lewis Sneddon <lewis@lewiscodes.io>
2023-07-23 09:08:16 +0100

    working on frontend closing open positions

--- webapp/src/app/position/position.component.ts
+++ webapp/src/app/position/position.component.ts
@@ -1,25 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Position } from './position.model';
 import { PositionService } from './position.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-position',
   templateUrl: './position.component.html',
 })
-export class PositionComponent implements OnInit {
+export class PositionComponent implements OnInit, OnDestroy {
 
   positions: Position[] = [];
+  private positionSubscription: Subscription | undefined;
 
   constructor(private readonly positionService: PositionService) {}
 
   ngOnInit(): void {
-    this.positionService.getPositions()
-      .subscribe(result => this.positions = result);
+    this.positionSubscription = this.positionService.getPositionObservable()
+      .subscribe(ps => {
+        this.positions = ps;
+      });
+    this.positionService.getPositions().subscribe();
   }
 
   closePositionHandler(id: number) {
-    this.positionService.closePosition(id)
-      .subscribe(res => console.log(res))
+    this.positionService.closePosition(id).subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.positionSubscription?.unsubscribe();
   }
 
 }
--- webapp/src/app/position/position.service.ts
+++ webapp/src/app/position/position.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, tap } from 'rxjs';
+import { Observable, Subject, tap } from 'rxjs';
 import { ClosedPosition, Position } from './position.model';
 import { environment } from '../../environments/environment';
 
@@ -12,12 +12,19 @@ export class PositionService {
   constructor(private readonly httpClient: HttpClient) { }
 
   private positions: Position[] = [];
+  private positionSubject = new Subject<Position[]>();
+
+  getPositionObservable(): Observable<Position[]> {
+    return this.positionSubject.asObservable();
+  }
 
   getPositions(): Observable<Position[]> {
     return this.httpClient.get<Position[]>(`${environment.apiUrl}/api/positions`)
       .pipe(
-        tap((positions: Position[]) => {
-          this.positions = positions;
+        tap((p: Position[]) => {
+          this.positions = p;
+          this.positionSubject.next(this.positions);
+          console.log(p);
         })
       );
   }
@@ -25,14 +32,9 @@ export class PositionService {
   closePosition(id: number): Observable<ClosedPosition> {
     return this.httpClient.post<ClosedPosition>(`${environment.apiUrl}/api/position/${id}/close`, {})
       .pipe(
-        tap((closedPosition: ClosedPosition) => {
-          // Find the position in the array by ID and update its properties with the received data.
-          const index = this.positions.findIndex(position => position.id === id);
-          if (index !== -1) {
-            this.positions[index].closePrice = closedPosition.closePrice;
-            this.positions[index].closedAt = closedPosition.closedAt;
-            // You can update other properties as needed based on the response from the server.
-          }
+        tap((cp: ClosedPosition) => {
+          this.positions = this.positions.filter(p => p.id !== cp.id)
+          this.positionSubject.next(this.positions);
         })
       );
   }
